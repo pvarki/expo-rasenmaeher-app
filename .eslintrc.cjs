7650fcfa42688b4eb7fef3bbd30bc64ccce8152a
Benjam <53127823+benjamsf@users.noreply.github.com>
2024-07-06 22:15:18 +0300

    include eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react-hooks/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react-refresh", "react-native"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
+  ignorePatterns: [
+    ".eslintrc.cjs",
+    "scripts/reset-project.js",
+    "src/mocks/**/*",
+    "mocksBrowser.ts",
+    "public/mockServiceWorker.js",
+    "babel.config.js",
+  ],
+};
